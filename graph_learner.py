771daf023d26d6ab1688a67249115f4815da9ddd
neitinifyy <104197603+neitinifyy@users.noreply.github.com>
2022-06-02 14:26:09 +0300

    Update graph_learner.py

--- graph_learner.py
+++ graph_learner.py
@@ -8,8 +8,6 @@ class GraphLearner:
 	def __init__(self, data, library_class, *args, **kwargs):
 
 		self._data = data
-		self._labels = list(self._data.columns)
-		self._adjacency_matrix = None
 		self._graph_dot = None
 		
 	def learn_graph(self):
